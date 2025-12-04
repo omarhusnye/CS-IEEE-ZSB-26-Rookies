cbe62e0d318871905b70bed60e8ce73aa1b92d71
Omar Hosny <omarhusnye6@gmail.com>
2025-12-04 21:10:29 +0200

    Add files via upload

---
+++ Task-1/F.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "codeforces";
+    int t; cin >> t;
+    while (t--) {
+     int a,b,c,d; cin >> a >> b >> c >> d;
+     if (a==b && b == c && c == d) cout << "YES" << endl;
+     else cout << "NO" << endl;
+
+    }
+    return 0;
+}
